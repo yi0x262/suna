041478277245eb4115c83f0ec3a9a232a63adfcc
yi0x262 <yutadefn@gmail.com>
2017-05-04 12:56:07 +0900

    test done

---
+++ gym_test.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python3
+
+import gym
+env = gym.make('MountainCar-v0')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print(reward,done,info)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
